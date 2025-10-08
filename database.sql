5b7a80adabd81814036c9e627a90182ca414799e
Iker <iciordia003@ikasle.ehu.eus>
2025-10-08 23:29:05 +0200

    Actualizado el fichero .sql
    He subido la base de datos con la estructura actualizada para que funcione bien con el código del commit anterior. Que se me había olvidado.

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 07-10-2025 a las 17:00:11
+-- Tiempo de generación: 08-10-2025 a las 21:28:42
 -- Versión del servidor: 10.8.2-MariaDB-1:10.8.2+maria~focal
 -- Versión de PHP: 8.2.27
 
@@ -29,21 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `usuario` text NOT NULL,
+  `usuario` text DEFAULT NULL,
   `nombre` text NOT NULL,
   `dni` text NOT NULL,
   `telefono` int(8) NOT NULL,
   `fechaNacimiento` date NOT NULL,
-  `email` text NOT NULL
+  `email` text NOT NULL,
+  `contrasena` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `nombre`, `dni`, `telefono`, `fechaNacimiento`, `email`) VALUES
-(1, '', 'mikel', '', 0, '0000-00-00', ''),
-(2, '', 'aitor', '', 0, '0000-00-00', '');
+INSERT INTO `usuarios` (`id`, `usuario`, `nombre`, `dni`, `telefono`, `fechaNacimiento`, `email`, `contrasena`) VALUES
+(1, '', 'mikel', '', 0, '0000-00-00', '', ''),
+(2, '', 'aitor', '', 0, '0000-00-00', '', '');
 
 --
 -- Índices para tablas volcadas
@@ -54,6 +55,16 @@ INSERT INTO `usuarios` (`id`, `usuario`, `nombre`, `dni`, `telefono`, `fechaNaci
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
